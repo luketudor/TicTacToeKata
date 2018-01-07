b67d534d601f877ded2ea27afa0270dc719208fd
luketudor <Luke.Tudor@myob.com>
2018-01-08 11:49:21 +1300

    Move player stuff to different files

---
+++ TicTacToe/Player.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace TicTacToe
+{
+    public class Player
+    {
+        public int TakeTurn(List<string> board)
+        {
+            return 0;
+        }
+    }
+}
---
+++ TicTacToe/Test/PlayerShould.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TicTacToe.Test
+{
+    [TestFixture]
+    public class PlayerShould
+    {
+        [Test]
+        public void ReturnAnyPlayerMoveForEmptyBoard()
+        {
+            var player = new Player();
+            var board = new List<string>();
+
+            var actualMove = player.TakeTurn(board);
+
+            Assert.Less(actualMove, 10);
+            Assert.GreaterOrEqual(actualMove, 0);
+        }
+    }
+}
