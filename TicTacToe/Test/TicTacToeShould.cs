76b24bbe6c75eeab0f4dbc98887d423d700a3e2c
luketudor <Luke.Tudor@myob.com>
2018-01-08 11:23:22 +1300

    Add first test for functionality

--- TicTacToe/Test/TicTacToeShould.cs
+++ TicTacToe/Test/TicTacToeShould.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace TicTacToe.Test
 {
@@ -9,6 +11,38 @@ namespace TicTacToe.Test
         public void TestTest()
         {
             Assert.Pass();
+        }
+
+        [Test]
+        public void ReturnAnyBoardAfterTurnOne()
+        {
+            var inputBoard = new Board(new string[9]);
+            var playerMove = 0;
+            var playerGlyph = "X";
+
+            var expectedBoard = new Board(new[]{"X"});
+
+            var actualBoard = inputBoard.AddGlyph(playerMove, playerGlyph);
+
+            CollectionAssert.AreEquivalent(expectedBoard, actualBoard);
+        }
+    }
+
+    public class Board : IEnumerable
+    {
+        private readonly string[] _glyphs;
+        public Board(string[] glyphs)
+        {
+            _glyphs = glyphs;
+        }
+        public Board AddGlyph(int index, string playerGlyph)
+        {
+            return new Board(new[] {"X"});
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _glyphs.GetEnumerator();
         }
     }
 }
