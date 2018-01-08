aa97782eeccfa189f2fff92340611929ae6a3165
luketudor <Luke.Tudor@myob.com>
2018-01-09 10:26:02 +1300

    Add board class

---
+++ TicTacToe/Board.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using NUnit.Framework.Api;
+
+namespace TicTacToe
+{
+    public class Board
+    {
+        private PlayerGlyph[] _boardState;
+        public int Count { get; }
+        public Board() : this(Enumerable.Repeat(PlayerGlyph.Empty, 9).ToArray(), 0)
+        {
+        }
+
+        internal Board(PlayerGlyph[] boardState, int count)
+        {
+            _boardState = boardState;
+            Count = count;
+        }
+
+    }
+}
--- TicTacToe/PlayerGlyph.cs
+++ TicTacToe/PlayerGlyph.cs
@@ -3,6 +3,7 @@
     public enum PlayerGlyph
     {
         Naught,
-        Cross
+        Cross,
+        Empty
     }
 }
