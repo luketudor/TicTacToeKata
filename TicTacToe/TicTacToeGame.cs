66297c018794817873b8de22144462d937dff6d2
luketudor <Luke.Tudor@myob.com>
2018-01-11 09:52:51 +1300

    Move win checking functionality to another class

--- TicTacToe/TicTacToeGame.cs
+++ TicTacToe/TicTacToeGame.cs
@@ -9,6 +9,7 @@ namespace TicTacToe
         private readonly IPlayer _player2;
         private readonly PlayerGlyph[] _currentBoard;
         private bool _player1Turn;
+        private readonly WinChecker _winChecker;
 
         public TicTacToeGame(IPlayer player1, IPlayer player2) : this(player1, player2,
             Enumerable.Repeat(PlayerGlyph.Empty, 9).ToArray(), true)
@@ -21,6 +22,7 @@ namespace TicTacToe
             _player2 = player2;
             _currentBoard = currentBoard;
             _player1Turn = player1Turn;
+            _winChecker = new WinChecker();
         }
 
         public PlayerGlyph[] NextBoard()
@@ -33,63 +35,12 @@ namespace TicTacToe
 
         public IPlayer Winner()
         {
-            return HasPlayerWon(_player1.GetGlyph()) ? _player1 : (HasPlayerWon(_player2.GetGlyph()) ? _player2 : null);
+            return _winChecker.HasPlayerWon(_currentBoard, _player1.GetGlyph()) ? _player1 : (_winChecker.HasPlayerWon(_currentBoard, _player2.GetGlyph()) ? _player2 : null);
         }
 
         public bool IsDraw()
         {
             return _currentBoard.All(cell => cell != PlayerGlyph.Empty);
-        }
-
-        private bool HasPlayerWon(PlayerGlyph player)
-        {
-            return WinningRows().Any(winningRow => winningRow.All(index => _currentBoard[index] == player));
-        }
-
-        private IEnumerable<IEnumerable<int>> WinningRows()
-        {
-            for (var i = 0; i < 3; i++)
-            {
-                yield return HorizontalRowIndices(i);
-            }
-            for (var i = 0; i < 3; i++)
-            {
-                yield return VerticalRowIndices(i);
-            }
-            yield return BackDiagonalRowIndices();
-            yield return ForwardDiagonalRowIndices();
-        }
-
-        private IEnumerable<int> HorizontalRowIndices(int rowNumber)
-        {
-            for (var j = 0; j < 3; j++)
-            {
-                yield return j + 3 * rowNumber;
-            }
-        }
-
-        private IEnumerable<int> VerticalRowIndices(int rowNumber)
-        {
-            for (var j = 0; j < 3; j++)
-            {
-                yield return j * 3 + rowNumber;
-            }
-        }
-
-        private IEnumerable<int> BackDiagonalRowIndices()
-        {
-            for (var j = 0; j < 3; j++)
-            {
-                yield return j * 4;
-            }
-        }
-
-        private IEnumerable<int> ForwardDiagonalRowIndices()
-        {
-            for (var j = 0; j < 3; j++)
-            {
-                yield return j * 2 + 2;
-            }
         }
     }
 }
---
+++ TicTacToe/WinChecker.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class WinChecker
+    {
+        private readonly IEnumerable<IEnumerable<int>> _winningRows;
+
+        public WinChecker()
+        {
+            IEnumerable<IEnumerable<int>> WinningRows()
+            {
+                IEnumerable<int> ForwardDiagonalRowIndices()
+                {
+                    for (var j = 0; j < 3; j++)
+                    {
+                        yield return j * 2 + 2;
+                    }
+                }
+                IEnumerable<int> BackDiagonalRowIndices()
+                {
+                    for (var j = 0; j < 3; j++)
+                    {
+                        yield return j * 4;
+                    }
+                }
+                IEnumerable<int> VerticalRowIndices(int rowNumber)
+                {
+                    for (var j = 0; j < 3; j++)
+                    {
+                        yield return j * 3 + rowNumber;
+                    }
+                }
+                IEnumerable<int> HorizontalRowIndices(int rowNumber)
+                {
+                    for (var j = 0; j < 3; j++)
+                    {
+                        yield return j + 3 * rowNumber;
+                    }
+                }
+                for (var i = 0; i < 3; i++)
+                {
+                    yield return HorizontalRowIndices(i);
+                    yield return VerticalRowIndices(i);
+                }
+                yield return BackDiagonalRowIndices();
+                yield return ForwardDiagonalRowIndices();
+            }
+
+            _winningRows = WinningRows();
+        }
+
+        public bool HasPlayerWon(PlayerGlyph[] currentBoard, PlayerGlyph player)
+        {
+            return _winningRows.Any(winningRow => winningRow.All(index => currentBoard[index] == player));
+        }
+
+    }
+}
