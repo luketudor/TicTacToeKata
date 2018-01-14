cd1a30af968324efc04e037deb91695360050035
luketudor <Luke.Tudor@myob.com>
2018-01-15 11:56:57 +1300

    Reduce duplication of win checker

--- TicTacToe/WinChecker.cs
+++ TicTacToe/WinChecker.cs
@@ -6,45 +6,49 @@ namespace TicTacToe
 {
     public class WinChecker
     {
-        private readonly IEnumerable<IEnumerable<int>> _winningRows;
+        private readonly IEnumerable<IEnumerable<int>> _winningRowsIndices;
 
         public WinChecker()
         {
+            var numHorizontalRows = 3;
+            var numVerticalCols = 3;
+            var numDiagonalRowLength = 3;
+
             IEnumerable<IEnumerable<int>> WinningRows()
             {
                 IEnumerable<int> ForwardDiagonalRowIndices()
                 {
-                    for (var j = 0; j < 3; j++)
+                    for (var i = 0; i < numDiagonalRowLength; i++)
                     {
-                        yield return j * 2 + 2;
+                        yield return i * 2 + 2;
                     }
                 }
 
                 IEnumerable<int> BackDiagonalRowIndices()
                 {
-                    for (var j = 0; j < 3; j++)
+                    for (var i = 0; i < numDiagonalRowLength; i++)
                     {
-                        yield return j * 4;
+                        yield return i * 4;
                     }
                 }
 
-                IEnumerable<int> VerticalRowIndices(int rowNumber)
+                IEnumerable<int> VerticalRowIndices(int horizontalRowNumber)
                 {
-                    for (var j = 0; j < 3; j++)
+                    for (var i = 0; i < numVerticalCols; i++)
                     {
-                        yield return j * 3 + rowNumber;
+                        yield return i * numVerticalCols + horizontalRowNumber;
                     }
                 }
 
-                IEnumerable<int> HorizontalRowIndices(int rowNumber)
+                IEnumerable<int> HorizontalRowIndices(int horizontalRowNumber)
                 {
-                    for (var j = 0; j < 3; j++)
+                    for (var i = 0; i < numVerticalCols; i++)
                     {
-                        yield return j + 3 * rowNumber;
+                        yield return i + numVerticalCols * horizontalRowNumber;
                     }
                 }
 
-                for (var i = 0; i < 3; i++)
+                for (var i = 0; i < numHorizontalRows; i++)
                 {
                     yield return HorizontalRowIndices(i);
                     yield return VerticalRowIndices(i);
@@ -53,12 +57,13 @@ namespace TicTacToe
                 yield return ForwardDiagonalRowIndices();
             }
 
-            _winningRows = WinningRows();
+            _winningRowsIndices = WinningRows();
         }
 
-        public bool HasPlayerWon(PlayerGlyph[] currentBoard, PlayerGlyph player)
-        {
-            return _winningRows.Any(winningRow => winningRow.All(index => currentBoard[index] == player));
-        }
+        public bool HasPlayerWon(PlayerGlyph[] currentBoard, PlayerGlyph player) => 
+            _winningRowsIndices.Any(winningRowIndices => 
+            winningRowIndices.All(winningRowIndex => 
+            currentBoard[winningRowIndex] == player
+            ));
     }
 }
