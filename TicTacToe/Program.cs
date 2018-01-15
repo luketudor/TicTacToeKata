f238fa3a79db31e7df2df21e9fe82846bb309d70
luketudor <Luke.Tudor@myob.com>
2018-01-15 16:11:39 +1300

    Use Chester's renderer for the game

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using TicTacToe.Enums;
+using TicTacToe.GameBoard;
 using TicTacToe.Players;
+using TicTacToe.Renderer;
 using TicTacToe.Renderers;
 
 namespace TicTacToe
@@ -9,6 +12,27 @@ namespace TicTacToe
     {
         public static void Main(string[] args)
         {
+            IEnumerable<Mark> Convert(PlayerGlyph[] board)
+            {
+                foreach(var glyph in board)
+                {
+                    switch(glyph)
+                    {
+                        case PlayerGlyph.Cross:
+                            yield return Mark.Cross;
+                            break;
+                        case PlayerGlyph.Naught:
+                            yield return Mark.Naught;
+                            break;
+                        case PlayerGlyph.Empty:
+                            yield return Mark.Empty;
+                            break;
+                        default:
+                            throw new Exception();
+                    }
+                }
+            }
+
             while (true)
             {
                 var game = new TicTacToeGame(
@@ -16,10 +40,11 @@ namespace TicTacToe
                             new TextPlayer(PlayerGlyph.Naught, Console.In, Console.Error)
                             );
                 var renderer = new TextBoardRenderer(Console.Out);
+                var ren2 = new ConsoleRenderer(); 
 
                 game.RaiseDrawEvent += (sender, eventArgs) => Console.WriteLine("Draw! Everyone loses!");
                 game.RaiseWinEvent += (sender, winner) => Console.WriteLine($"Congratulations, {winner.GetGlyph()} player won!");
-                game.RaiseRenderEvent += (sender, board) => renderer.Render(board);
+                game.RaiseRenderEvent += (sender, board) => ren2.RenderBoard(new List<Mark>(Convert(board)));
 
                 game.Run();
 
